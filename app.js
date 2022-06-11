6105b455eb8e2768c3c9840c6c5bde44e3e5e383
muhammad naveed <amirprinceak5@gmail.com>
2022-06-11 21:18:24 +0500

    complete

---
+++ app.js
@@ -0,0 +1,136 @@
+//Q-1
+// var arr = [];
+// console.log(arr);
+
+
+//Q-2 
+//  var arr = [];
+//  arr[0] = "muhammad";
+//  arr[1] = "naveed";
+//  arr[2] = "ali";
+//  arr[3] = "Amir";
+//  console.log(arr);
+
+// Q-3
+
+// var x;
+// for (x=1; x<=10; x++){
+//     console.log(x)
+
+// }
+
+ 
+
+// var tableNumber = +prompt(" enter table number");
+// var tableLength = +prompt(" enter table lenght");
+
+// if (tableNumber){
+//   tableNumber = tableNumber ;
+// } else {
+//   tableNumber =5;
+// }
+// if (tableLength){
+//   tableLength = tableLength;
+// } else {
+//   tableLength = 10;
+// }
+// for (var i = 1; i <= tableLength; i++) {
+//   document.write(
+//     tableNumber + " " + "x" + " " + i + " = " + tableNumber * i + "<br />");
+// }
+
+
+//Q- 5
+
+// var fruits = [ "apple", "banana", "mango", "orange", "strawberry"];
+// console.log(fruits);
+
+//Q-6
+// COUNTING
+// var x;
+//  for (x=1; x<=15; x++){
+//      console.log(x)
+
+//  }
+
+//REVERSE COUNTING
+
+// var x;
+//   for (x=10; x>=1; x--){
+//       console.log(x)
+
+//   }
+
+// EVEN COUNTING
+
+//var x;
+  //for (x=0; x<=100; x= x+5){
+    //  console.log(x)
+     
+     //}
+
+//ODD COUNTING
+// var x;
+//  for (x=1; x<=20; x= x+2){
+//      console.log(x)
+     
+//     }
+
+//Q-7 
+//  var string = prompt("wellcome to  ABC bakery what do you want to order sir/mam ?");
+//  string = string.toLowerCase();
+// var fruits = [ "cake", "apple pie", "cookie", "chips", "patties"];
+// for (var i = 0; i <= 4; i++) {
+//   if (string === fruits [i]) {
+//     alert("its available ");
+//     break
+
+//   } else {
+//     alert("we are sorry is not available in our bakery ");
+//   break
+//   }
+// }
+
+//console.log(var);
+
+
+
+//Q-8
+//  var arr = [21, 53, 78, 91, 12];
+
+//  var max = arr[0];
+
+// for(var i = 0; i < arr.length; i++) {
+//   if (arr[i] >= max) {
+//   max = arr[i];
+//   console.log(max);
+//     }
+// }
+
+// console.log("largest number is : " + max);
+
+//Q-9
+
+// var arr = [21, 53, 78, 91, 100, 99, 200, 12];
+
+// var min = arr[0];
+
+// for(var i = 0; i < arr.length; i++) {
+//  if (arr[i] < min) {
+//  min = arr[i];
+//  console.log(min);
+//    }
+// }
+
+// console.log("The smallest number is : " + min);
+
+
+//Q-10
+// var a;
+//   for (a=0; a<=100; a= a+5){
+//       console.log(a)
+     
+    // }
+
+
+
